95f290b52ff1ae6de98f69bb848d3a191df789bc
朱宏博 <1076306278@qq.com>
2019-08-09 15:25:17 +0800

    Initial commit

---
+++ service-hi/src/main/java/com/example/service_hi/controller/SayHelloController.java
@@ -0,0 +1,21 @@
+package com.example.service_hi.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * All Rights Reserved, Designed By ZHB.
+ *
+ * @author:
+ * @date:
+ * @copyright:
+ */
+@RestController
+public class SayHelloController {
+
+    @RequestMapping(value = "/hello/{name}")
+    public String sayHello(@PathVariable("name")String name){
+        return "Hello,".concat(name).concat("!");
+    }
+}
---
+++ service-ribbon/src/main/java/com/cxgc/serviceribbon/controller/AskController.java
@@ -0,0 +1,40 @@
+package com.cxgc.serviceribbon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * All Rights Reserved, Designed By ZHB.
+ *
+ * @author:
+ * @date:
+ * @copyright:
+ */
+@RestController
+@Configuration
+public class AskController {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Value("${spring.application.name}")
+    private String name;
+
+    @RequestMapping(value = "/ask")
+    public String ask() {
+        //RestTemplate将根据服务名eureka-client-service通过预先从eureka-service（即服务注册中心）缓存到本地的注册表中获取到eureka-client-service服务的具体地址，从而发起服务间调用
+        String askHelloFromService = restTemplate.getForEntity("http://EUREKA-CLIENT-SERVICE/hello/{name}", String.class, name).getBody();
+        return askHelloFromService;
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
